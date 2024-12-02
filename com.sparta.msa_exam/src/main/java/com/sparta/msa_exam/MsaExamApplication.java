933387548c745592f4282d6e9a45cf563c23c813
ykybkoft <126169447+ykybkoft@users.noreply.github.com>
2024-12-02 19:55:03 +0900

    ✨feat : 기본 구조 commit

---
+++ com.sparta.msa_exam/src/main/java/com/sparta/msa_exam/MsaExamApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.msa_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaExamApplication.class, args);
+	}
+
+}
